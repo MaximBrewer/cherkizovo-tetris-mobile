fa99cef654cbd4c9a12b1784959d937eb742f69c
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-02 21:45:57 +0600

    vbv

--- src/panels/Intro.js
+++ src/panels/Intro.js
@@ -12,19 +12,21 @@ const Intro = ({ id, go, route, fetchedUser }) => {
 		<Panel id={id}>
 			{fetchedUser &&
 				<Fragment>
-					<Div className='Intro' style={{ textAlign: "center", backgroundImage: 'url(' + bg + ')', display: "flex", justifyContent: "center", alignItems: "center" }} >
-						<Div style={{ minHeight: "70%", color: "#ffffff" }}>
-							<h2>Новый год с</h2>
-							<h2><img src={logo} alt="" style={{ width: "12rem" }} /></h2>
-							<h3>Миссия: <br /> накрой праздничный стол с&nbsp;Черкизово!</h3>
-							<p>Набери больше всех баллов и&nbsp;получи крутые призы!</p>
-							<p>&nbsp;</p>
-							<p><button className={`btn-red`} onClick={() => go(route)}>Играть</button></p>
-						</Div>
+					<Div className='Intro' style={{ paddingTop: "216%", position: "relative" }} >
+						<div style={{ textAlign: "center", backgroundImage: 'url(' + bg + ')', height: "100%", width: "100%", position: "absolute", top: 0, left: 0, justifyContent: "center", alignItems: "center", display: "flex" }}>
+							<Div style={{ minHeight: "70%", color: "#ffffff" }}>
+								<h2>Новый год с</h2>
+								<h2><img src={logo} alt="" style={{ width: "12rem" }} /></h2>
+								<h3>Миссия: <br /> накрой праздничный стол с&nbsp;Черкизово!</h3>
+								<p>Набери больше всех баллов и&nbsp;получи крутые призы!</p>
+								<p>&nbsp;</p>
+								<p><button className={`btn-red`} onClick={() => go(route)}>Играть</button></p>
+							</Div>
+						</div>
 					</Div>
 				</Fragment>
 			}
-		</Panel>
+		</Panel >
 	)
 }
 export default Intro;
