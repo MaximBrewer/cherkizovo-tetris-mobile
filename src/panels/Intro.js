9755993bf698917e37301acc5aeae5a60ea5e5ee
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-02 21:59:30 +0600

    sdsd

--- src/panels/Intro.js
+++ src/panels/Intro.js
@@ -12,18 +12,20 @@ const Intro = ({ id, go, route, fetchedUser }) => {
 		<Panel id={id}>
 			{fetchedUser &&
 				<Fragment>
-					<Div className='Intro' >
-						<div style={{ backgroundImage: 'url(' + bg + ')' }}>
-							<Div style={{ minHeight: "70%", color: "#ffffff" }}>
-								<h2>Новый год с</h2>
-								<h2><img src={logo} alt="" style={{ width: "12rem" }} /></h2>
-								<h3>Миссия: <br /> накрой праздничный стол с&nbsp;Черкизово!</h3>
-								<p>Набери больше всех баллов и&nbsp;получи крутые призы!</p>
-								<p>&nbsp;</p>
-								<p><button className={`btn-red`} onClick={() => go(route)}>Играть</button></p>
-							</Div>
-						</div>
-					</Div>
+					<div style={{ maxHeight: "100vh", overflow: "hidden" }}>
+						<Div className='Intro'>
+							<div style={{ backgroundImage: 'url(' + bg + ')' }}>
+								<Div style={{ minHeight: "70%", color: "#ffffff" }}>
+									<h2>Новый год с</h2>
+									<h2><img src={logo} alt="" style={{ width: "12rem" }} /></h2>
+									<h3>Миссия: <br /> накрой праздничный стол с&nbsp;Черкизово!</h3>
+									<p>Набери больше всех баллов и&nbsp;получи крутые призы!</p>
+									<p>&nbsp;</p>
+									<p><button className={`btn-red`} onClick={() => go(route)}>Играть</button></p>
+								</Div>
+							</div>
+						</Div>
+					</div>
 				</Fragment>
 			}
 		</Panel >
