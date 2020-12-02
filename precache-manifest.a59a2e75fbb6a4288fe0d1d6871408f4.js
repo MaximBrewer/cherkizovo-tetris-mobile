41f57e282e643b6e56d62f6eb5ad13c55ec0de54
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-02 21:06:34 +0600

    Updates

---
+++ precache-manifest.a59a2e75fbb6a4288fe0d1d6871408f4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61dffab4167798dd37dae8ffd6fffdb4",
+    "url": "/cherkizovo-tetris-mobile/index.html"
+  },
+  {
+    "revision": "5637815daa1eab0cd7ca",
+    "url": "/cherkizovo-tetris-mobile/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "a1eb681fbed1e39b2836",
+    "url": "/cherkizovo-tetris-mobile/static/css/main.d3afd78a.chunk.css"
+  },
+  {
+    "revision": "5637815daa1eab0cd7ca",
+    "url": "/cherkizovo-tetris-mobile/static/js/2.8dbc3da9.chunk.js"
+  },
+  {
+    "revision": "fa8476a28755c72e9a88d42d463763f3",
+    "url": "/cherkizovo-tetris-mobile/static/js/2.8dbc3da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1eb681fbed1e39b2836",
+    "url": "/cherkizovo-tetris-mobile/static/js/main.aaf9190c.chunk.js"
+  },
+  {
+    "revision": "1a9787e7ec2b1e994931",
+    "url": "/cherkizovo-tetris-mobile/static/js/runtime-main.44675682.js"
+  },
+  {
+    "revision": "06e2c93fed660c180a371b7fc0e9d6b9",
+    "url": "/cherkizovo-tetris-mobile/static/media/canvas-bg.06e2c93f.png"
+  },
+  {
+    "revision": "1a5dfd38609ccb5f463e1b54a5a3c4ad",
+    "url": "/cherkizovo-tetris-mobile/static/media/game-bg.1a5dfd38.jpg"
+  },
+  {
+    "revision": "e4f61210dfbbb2d81f5073432fbac721",
+    "url": "/cherkizovo-tetris-mobile/static/media/intro-bg.e4f61210.jpg"
+  },
+  {
+    "revision": "19fa04fb4f758f2488d1c67b3671a20b",
+    "url": "/cherkizovo-tetris-mobile/static/media/logo-white.19fa04fb.png"
+  },
+  {
+    "revision": "18ca30f90316094026291f079957e3cc",
+    "url": "/cherkizovo-tetris-mobile/static/media/music.18ca30f9.mp3"
+  },
+  {
+    "revision": "e3c35f0936d3d00cb78508211e674ba9",
+    "url": "/cherkizovo-tetris-mobile/static/media/woop.e3c35f09.mp3"
+  }
+]);
