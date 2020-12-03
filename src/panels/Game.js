d057374d417cf002d76706689afd0ced6076ffbd
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-04 01:47:16 +0600

    sds

--- src/panels/Game.js
+++ src/panels/Game.js
@@ -178,6 +178,7 @@ const Game = ({ id, go, route, fetchedUser, activePanel }) => {
 				<Fragment>
 					<Div className='Game' style={{ textAlign: "center", backgroundImage: 'url(' + bg + ')', color: "#ffffff" }} >
 						<Div>
+							<h2><img src={logo} alt="" style={{ width: "12rem", maxWidth: "50vw" }} /></h2>
 							<h5>Миссия: “Накрой праздничный стол”</h5>
 							{/* <p>Набери больше всех баллов и&nbsp;получи крутые призы!</p> */}
 						</Div>
