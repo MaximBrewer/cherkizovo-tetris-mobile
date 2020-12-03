42365165d4314553976af60c202303546e79e6e9
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-04 01:26:01 +0600

    sdsd

--- src/panels/Intro.js
+++ src/panels/Intro.js
@@ -25,7 +25,18 @@ const Intro = ({ id, go, route, fetchedUser }) => {
 									<h2><img src={logo} alt="" style={{ width: "12rem" }} /></h2>
 									<h3>Миссия: <br />“Накрой праздничный стол с&nbsp;Черкизово!”</h3>
 									<p><b>Набери больше всех баллов <br />и&nbsp;получи крутые призы!</b></p>
-									<p><img src={galkin} alt="Максим Галкин" style={{ maxWidth: "20rem", width: "70%" }} /></p>
+									<div style={{ position: "relative" }}>
+										<div style={{
+											background: "#AA2223",
+											border: "2px solid #FFFFFF",
+											borderRadius: "3px",
+											transform: "rotate(-5.95deg)",
+											maxWidth: "16rem",
+											width: "60%",
+											position: "relative"
+										}}>Попробуй набрать больше баллов,<br />чем Максим Галкин</div>
+										<img src={galkin} alt="Максим Галкин" style={{ maxWidth: "20rem", width: "70%", position: "relative", zIndex: 1 }} />
+									</div>
 									<p><button className={`btn-red`} onClick={() => go(route)}>Играть</button></p>
 								</Div>
 							</div>
